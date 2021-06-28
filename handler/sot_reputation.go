46055972946c7d95e2defe45ff760e733784f872
Winni Neessen <wn@neessen.net>
2021-06-28 14:59:23 +0200

    Reputation assets

--- handler/sot_reputation.go
+++ handler/sot_reputation.go
@@ -17,17 +17,6 @@ func GetSotReputation(h *http.Client, u *user.User, f string) (*discordgo.Messag
 		"action": "handler.GetSotReputation",
 	})
 
-	thumbUrls := map[string]string{
-		"athena":   "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/c/c2/Athena%27s_Fortune_icon.png/revision/latest/scale-to-width-down/152?cb=20200512034100",
-		"bilge":    "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/5/5e/Bilge_rat_adventures.png/revision/latest/scale-to-width-down/250?cb=20180710203959",
-		"hoarder":  "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/f/f1/Gold_Hoarders_icon.png/revision/latest/scale-to-width-down/152?cb=20200512034953",
-		"hunter":   "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/b/b1/The_Hunter%27s_Call_icon.png/revision/latest/scale-to-width-down/152?cb=20200920220917",
-		"merchant": "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/9/9c/Merchant_Alliance_icon.png/revision/latest/scale-to-width-down/152?cb=20200512032051",
-		"order":    "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/0/02/Order_of_Souls_icon.png/revision/latest/scale-to-width-down/152?cb=20200512154422",
-		"reaper":   "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/6/61/Reaper%27s_Bones_icon.png/revision/latest/scale-to-width-down/152?cb=20200512033231",
-		"seadog":   "https://static.wikia.nocookie.net/seaofthieves_gamepedia/images/3/3c/Sea_Dogs_icon.png/revision/latest/scale-to-width-down/152?cb=20200919191401",
-	}
-
 	wrongFormatMsg := fmt.Sprintf("Incorrect request format. Usage: !reputation " +
 		"<athena|bilge|hoarder|hunter|merchant|order|reaper|seadog>")
 	validFaction, err := regexp.Compile(`^(athena|bilge|hoarder|hunter|merchant|order|reaper|seadog)$`)
@@ -72,7 +61,7 @@ func GetSotReputation(h *http.Client, u *user.User, f string) (*discordgo.Messag
 		Title:       fmt.Sprintf("%v reputation summary for @%v", userReputation.Name, u.AuthorName),
 		Description: fmt.Sprintf("*%q*", userReputation.Motto),
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL: thumbUrls[factionName],
+			URL: fmt.Sprintf("https://github.com/wneessen/sotbot/raw/main/assets/reputation/%s.png", factionName),
 		},
 		Fields: emFields,
 	}
