32cebe741f855e16eeb1d5323206c4cedfcf8fdb
Winni Neessen <winfried.neessen@cleverbridge.com>
2022-05-30 19:13:45 +0200

    v1.4.9-DEV: Goldensands faction IDs were mixed up

--- handler/sot_goldensands.go
+++ handler/sot_goldensands.go
@@ -28,14 +28,14 @@ func GetSotGoldenSands(h *http.Client, u *user.User) (*discordgo.MessageEmbed, e
 		Title:       "The battle for Golden Sands Outpost",
 		Description: "The situation is currently neutral. Neither the Reaper's nor Merrick's team is in the lead.",
 	}
-	if goals.LeadingGroupGoalID == "1463b6ce-7e0e-4b10-9765-ccff3d9a8152" {
+	if goals.LeadingGroupGoalID == "f08b4609-0949-46c5-8fc9-3202d743474a" {
 		responseEmbed.Description = "The force for the Flame is strong. The Reaper's are currently in the lead and on their way to burn down Golden Sands Outpost"
 		responseEmbed.Image = &discordgo.MessageEmbedImage{
 			URL: "https://compass-ssl.xboxlive.com/assets/d8/4a/d84a2fac-a33e-4ba7-9ad0-8cd9e91a745e.jpg",
 		}
 		responseEmbed.Type = discordgo.EmbedTypeImage
 	}
-	if goals.LeadingGroupGoalID == "f08b4609-0949-46c5-8fc9-3202d743474a" {
+	if goals.LeadingGroupGoalID == "1463b6ce-7e0e-4b10-9765-ccff3d9a8152" {
 		responseEmbed.Description = "Merrick's builder alliance is strong. Currently they are in the lead and on their way to rebuild Golden Sands Outpost"
 		responseEmbed.Image = &discordgo.MessageEmbedImage{
 			URL: "https://compass-ssl.xboxlive.com/assets/66/26/6626bd7f-fe14-403b-86b1-8f91d7006555.jpg",
